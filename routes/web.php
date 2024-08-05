d55a4a3ef30e53bbe6052215118e6f434610b0b6
laillaasri <laillaasri@gmail.com>
2024-08-05 20:17:15 +0700

    [PUSH] Blade Component

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', ['title' => 'Home Page']);
 });
 
-Route::get('/home', function () {
-    return view('home');
+Route::get('/about', function () {
+    return view('about', ['title' => 'Lailla Asri']);
+});
+
+Route::get('/blog', function () {
+    return view('blog', ['title' => 'Blog']);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact']);
 });
