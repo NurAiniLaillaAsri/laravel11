23d2412b291bec98f98b4341f91d1c50d406abea
laillaasri <laillaasri@gmail.com>
2024-08-07 20:42:58 +0700

    [UPDATE] view data from router

--- resources/views/blog.blade.php
+++
@@ -1,4 +0,0 @@
-<x-layout>
-    <x-slot:title>{{ $title }}</x-slot:title>     {{-- $title di sini mengambil dari route  --}}
-    <h1>{{ $title }}</h1>
-</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,21 @@ Route::get('/about', function () {
     return view('about', ['title' => 'Lailla Asri']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', [
+        'title' => 'Blog',
+        'posts' => [
+            [
+                'title' => 'Judul Artikel 1',
+                'author' => 'Nur Aini',
+                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid tenetur quia iste alias. Nam, dolore dolor minima nesciunt alias rerum quis a accusamus et. Quos blanditiis amet magnam tenetur nobis.'
+            ],[
+                'title' => 'Judul Artikel 2',
+                'author' => 'Lailla Asri',
+                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid tenetur quia iste alias. Nam, dolore dolor minima nesciunt alias rerum quis a accusamus et. Quos blanditiis amet magnam tenetur nobis.'
+            ],
+        ]
+    ]);
 });
 
 Route::get('/contact', function () {
