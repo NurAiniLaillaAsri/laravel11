7ea5aebbdce6c57c4b270303c6a45d8fcc675749
laillaasri <laillaasri@gmail.com>
2024-08-16 21:07:15 +0700

    [UPDATE] add eager loading by default

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,6 +9,9 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Post extends Model {          // otomatis terhubung dengan tabel posts
     use HasFactory;
     protected $fillable = ['title', 'author', 'slug', 'body'];
+
+    // Eager Loading by Default
+    protected $with = ['author', 'category'];
     
     public function author():BelongsTo {
         return $this->belongsTo(User::class);
--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,17 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    $post = Post::with(['author', 'category'])->latest()->get();        // Eager loading, mengurangi penggunaan query
+    // Eager loading, mengurangi penggunaan query
+    // $post = Post::with(['author', 'category'])->latest()->get();        
+    // return view('posts', [
+    //     'title' => 'Blog',
+    //     'posts' => $post
+    // ]);
+
+    $post = Post::latest()->get();
     return view('posts', [
         'title' => 'Blog',
-        'posts' => $post
+        'posts' => $post,
     ]);
 });
 
@@ -27,15 +34,21 @@ Route::get('/posts/{post:slug}', function (Post $post) {        // Eloquent mode
 });
 
 Route::get('/authors/{user:username}', function (User $user) {  
-    $post = $user->posts->load('category', 'author');           // Lazy eager loading, Sometimes you may need to eager load a relationship after the parent model has already been retrieved.
+    // Lazy eager loading, Sometimes you may need to eager load a relationship after the parent model has already been retrieved.
+    // $post = $user->posts->load('category', 'author');           
 
-    return view('posts', ['title' => count($post) . ' Articles by ' . $user->name, 'posts' => $post]);
+    // return view('posts', ['title' => count($post) . ' Articles by ' . $user->name, 'posts' => $post]);
+
+    return view('posts', ['title' => count($user->posts) . ' Articles by ' . $user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {  
-    $post = $category->posts->load('category', 'author');           // Lazy eager loading, Sometimes you may need to eager load a relationship after the parent model has already been retrieved.
+    // Lazy eager loading, Sometimes you may need to eager load a relationship after the parent model has already been retrieved.
+    // $post = $category->posts->load('category', 'author');           
 
-    return view('posts', ['title' => 'Articles in ' . $category->name, 'posts' => $post]);
+    // return view('posts', ['title' => 'Articles in ' . $category->name, 'posts' => $post]);
+
+    return view('posts', ['title' => 'Articles in ' . $category->name, 'posts' => $category->posts]);
 });
 
 Route::get('/contact', function () {
